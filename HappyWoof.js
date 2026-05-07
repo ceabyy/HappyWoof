a2ffaec0f96742a27d1bfb10b328de210f0b937b
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-05-08 10:59:36 +1200

    fixed html sources

--- HappyWoof.js
+++ HappyWoof.js
@@ -1,3 +1,7 @@
+const BASE_URL = window.location.hostname === '127.0.0.1'
+    ? 'http://127.0.0.1:5000'
+    : 'https://happywoof.onrender.com';
+
 // buttons on page
 const fileButton = document.getElementById("fileButton")
 const submitButton = document.getElementById("submitButton")
