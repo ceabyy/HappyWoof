207c3c6cb37ab5e3db230e891f626def1b26422c
Ceaby Fernandez <ceabyfernandez@Ceabys-MacBook-Pro.local>
2026-05-08 10:24:27 +1200

    changed python file for render

--- HappyWoof.py
+++ HappyWoof.py
@@ -65,4 +65,5 @@ def predictDog(): #using file from page
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT, 5000')))
